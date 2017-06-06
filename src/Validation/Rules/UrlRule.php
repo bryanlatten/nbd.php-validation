edf05b2f0aa241854b0dd0eecea9a35a12b796d0
Mike Sherov <mike.sherov@gmail.com>
2017-06-06 17:23:10 -0400

    Urls: don’t allow schemes unless they are mailto, http, or https

--- src/Validation/Rules/UrlRule.php
+++ src/Validation/Rules/UrlRule.php
@@ -11,4 +11,17 @@ class UrlRule extends RegexRuleAbstract {
 
   protected $_pattern = '/^((?:https?):\/\/)?(?:(?:(?:[\w\.\-\+!$&\'\(\)*\+,;=_]|%[0-9a-f]{2})+:)*(?:[\w\.\-\+%!$&\'\(\)*\+,;=_]|%[0-9a-f]{2})+@)?(?:[A-Za-z0-9_\-]+\.)(?:[A-Za-z0-9_\-\.])+(?::[0-9]+)?(?:[\/|\?](?:[\w#!:\.\?\+=&@$\'~*,;_\/\(\)\[\]\-]|%[0-9a-f]{2})*)?$/ui';
 
+  /**
+   * @inheritDoc
+   */
+  public function isValid( $data, array $context = null ) {
+
+    if ( !parent::isValid( $data, $context ) ) {
+      return false;
+    }
+
+    return in_array( parse_url( $data, PHP_URL_SCHEME ), ['', 'http', 'https', 'mailto'] );
+
+  } // isValid
+
 } // UrlRule
--- tests/Validation/Rules/UrlRuleTest.php
+++ tests/Validation/Rules/UrlRuleTest.php
@@ -26,6 +26,7 @@ class UrlRuleTest extends \PHPUnit_Framework_TestCase {
   public function testDataProvider() {
 
     return [
+        // allowed
         [ 'www.google.com', true ],
         [ 'bob@behance.com', true ],
         [ 'mailto:dave@behance.com', true ],
@@ -33,11 +34,38 @@ class UrlRuleTest extends \PHPUnit_Framework_TestCase {
         [ 'https://www.google.com', true ],
         [ 'www.google.com?a=1&b=2#fun', true ],
         [ 'www.behance.net/job/Multimedia-Designer%2C-Senior', true ], // testing "%2C"
+        // disallowed: protocol relative
         [ '//www.google.com', false ],
         [ '//www.google.co.uk', false ],
+        // disallowed: non url encoded
         [ 'www.google.com?b=2|3', false ],
+        // disallowed: archaic protocols
+        [ 'magnet:www.google.com', false ],
+        [ 'magnet://www.google.com', false ],
+        [ 'ftp:www.google.com', false ],
+        [ 'ftp://www.google.com', false ],
+        [ 'wss:www.google.com', false ],
+        [ 'wss://www.google.com', false ],
+        [ 'ws:www.google.com', false ],
+        [ 'ws://www.google.com', false ],
+        // disallowed: xss
         [ 'javascript:alert("dave@behance.com")', false ],
+        [ 'javascript:alert(1);www.google.com', false ],
+        [ 'javascript:alert(1);x=\'@www.google.com\'', false ],
+        [ 'javascript:alert(1);x=\'@www.google.com?\'', false ],
+        [ ' javascript:alert("dave@behance.com")', false ],
+        [ ' javascript:alert(1);www.google.com', false ],
+        [ ' javascript:alert(1);x=\'@www.google.com\'', false ],
+        [ ' javascript:alert(1);x=\'@www.google.com?\'', false ],
         [ 'java\0script:alert("dave@behance.com")', false ],
+        [ 'java\0script:alert(1);www.google.com', false ],
+        [ 'java\0script:alert(1);x=\'@www.google.com\'', false ],
+        [ 'java\0script:alert(1);x=\'@www.google.com?\'', false ],
+        [ 'java&#x0D;script:alert("dave@behance.com")', false ],
+        [ 'java&#x0D;script:alert(1);www.google.com', false ],
+        [ 'java&#x0D;script:alert(1);x=\'@www.google.com\'', false ],
+        [ 'java&#x0D;script:alert(1);x=\'@www.google.com?\'', false ],
+        // disallowed: non strings
         [ 123.0e26, false ],
         [ null, false ],
         [ false, false ],
